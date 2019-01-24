b65fb8d072d002bd5d73ad36d80259a74de8c3b0
cu <cu@caesar.team>
2019-01-24 14:13:06 +0300

    [Date] Changed dateFormatter typeFrom

--- common/utils/dateFormatter.js
+++ common/utils/dateFormatter.js
@@ -1,7 +1,5 @@
 import { DateTime } from 'luxon';
 
 export function formatDate(date, dateFormatTo = 'LLL dd, yyyy hh:mm a') {
-  return DateTime.fromFormat(date, 'LLL dd, yyyy hh:mm a').toFormat(
-    dateFormatTo,
-  );
+  return DateTime.fromISO(date).toFormat(dateFormatTo);
 }

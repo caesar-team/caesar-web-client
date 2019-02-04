10ed647bf0f25f1a893932c04d017acbb52ca430
iabramov4xxi <iabramov@caesar.team>
2019-02-04 15:22:35 +0300

    [Item] CAES-222: fixed attachments and remove button styles

--- components/Item/Types/components/RemoveButtonWrapper.js
+++ components/Item/Types/components/RemoveButtonWrapper.js
@@ -3,5 +3,5 @@ import styled from 'styled-components';
 export const RemoveButtonWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
-  margin-top: 50px;  
+  margin-top: 20px;
 `;

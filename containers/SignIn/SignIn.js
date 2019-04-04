431ec0627ad757f8dc2b6551467e210fbd539099
dspiridonov4xxi <dspiridonov@4xxi.com>
2019-04-04 15:55:49 +0500

    [CAES-377] Removed unused styles

--- containers/SignIn/SignIn.js
+++ containers/SignIn/SignIn.js
@@ -13,55 +13,12 @@ import {
   AuthTitle,
   AuthDescription,
   Button,
-  TextWithLines, AuthLayout,
+  TextWithLines,
+  AuthLayout,
 } from 'components';
 import { login } from 'common/utils/authUtils';
 import { getTrustedDeviceToken, setToken } from 'common/utils/token';
-import BgRightImg from 'static/images/bg-right.jpg';
-import BgRightImg2x from 'static/images/bg-right@2x.jpg';
-import BgLeftImg from 'static/images/bg-left.jpg';
-import BgLeftImg2x from 'static/images/bg-left@2x.jpg';
 import SignInForm from './SignInForm';
-import SignUpForm from '../SignUp/SignUpForm';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 60px;
-  background: ${({ theme }) => theme.white};
-`;
-
-const InnerWrapper = styled.div`
-  width: 400px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-  min-height: calc(100vh - 240px);
-`;
-
-const TopWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-`;
-
-const BgRightImage = styled.img`
-  position: absolute;
-  right: 60px;
-  object-fit: contain;
-`;
-
-const BgLeftImage = styled.img`
-  position: absolute;
-  left: 60px;
-  bottom: 90px;
-  object-fit: contain;
-`;
 
 const IconWrapper = styled.div`
   display: flex;
@@ -108,27 +65,6 @@ const StyledButton = styled(Button)`
   letter-spacing: 0.6px;
   padding: 18px 30px;
   height: 60px;
-`;
-
-const BottomWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 40px;
-`;
-
-const FourXXIText = styled.div`
-  font-size: 12px;
-  font-weight: 600;
-  letter-spacing: 0.5px;
-  color: ${({ theme }) => theme.black};
-  margin-left: 10px;
-`;
-
-const FourXXILink = styled.a`
-  text-decoration: underline;
-  font-weight: 600;
-  color: ${({ theme }) => theme.black};
 `;
 
 class SignInContainer extends Component {

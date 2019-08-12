cfb8215db8ca54806f23513698304d4e22ad43a8
dspiridonov4xxi <dspiridonov@4xxi.com>
2019-08-12 17:41:01 +0500

    [CAES-504] Removed dependency from ManageList

--- containers/ManageList/ManageList.js
+++ containers/ManageList/ManageList.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { ManageList, ListFormModal, ConfirmModal, Button, FullScreenLoader, TextLoader } from 'components';
+import {
+  ManageList,
+  ListFormModal,
+  ConfirmModal,
+  Button,
+  TextLoader,
+} from 'components';
 import {
   LIST_WORKFLOW_EDIT_MODE,
   LIST_WORKFLOW_CREATE_MODE,

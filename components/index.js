63741851a294a52fb74d9b4c89aaf4a5a7df23a0
Dmitry Spiridonov <dspiridonov@4xxi.com>
2019-08-13 18:10:08 +0500

    [CAES-257] Added LongClickBehaviour component. Fixed components which… (#171)
    * [CAES-257] Added LongClickBehaviour component. Fixed components which is using it
    
    * [CAES-257] Removed handler
    
    * [CAES-257] Renamed LongClick to HoldClick

--- components/index.js
+++ components/index.js
@@ -70,3 +70,4 @@ export { Toggle } from './Toggle';
 export { RangeInput } from './RangeInput';
 export { PasswordIndicator } from './PasswordIndicator';
 export { TagsInput } from './TagsInput';
+export { HoldClickBehaviour } from './HoldClickBehaviour';

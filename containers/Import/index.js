21af0d139ca8394afdf54600801c78c4b90f7164
Dmitry <dspiridonov@4xxi.com>
2019-10-10 13:01:38 +0100

    [CAES-650] Added opportunity to specify team and list of importing data

--- containers/Import/index.js
+++ containers/Import/index.js
@@ -3,13 +3,13 @@ import { createItemsBatchRequest } from 'common/actions/entities/item';
 import { initWorkflow } from 'common/actions/workflow';
 import { fetchKeyPairRequest, fetchUserSelfRequest } from 'common/actions/user';
 import { createStructuredSelector } from 'reselect';
-import { selectableListsWithoutChildrenSelector } from 'common/selectors/entities/list';
+import { selectableTeamsListsSelector } from 'common/selectors/entities/list';
 import { keyPairSelector } from 'common/selectors/user';
 import { isLoadingSelector } from 'common/selectors/workflow';
 import Import from './Import';
 
 const mapStateToProps = createStructuredSelector({
-  lists: selectableListsWithoutChildrenSelector,
+  teamsLists: selectableTeamsListsSelector,
   keyPair: keyPairSelector,
   isLoading: isLoadingSelector,
 });

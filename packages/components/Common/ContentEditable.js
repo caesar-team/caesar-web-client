303a3e0c52e5f7e3fddd47c9eb48324bb228d5ab
Sergey Alekseyev <sa@4xxi.com>
2020-06-30 17:53:35 +0600

    [Items] CAES-766: Fixed after cr
    Signed-off-by: Sergey Alekseyev <sa@4xxi.com>

--- packages/components/Common/ContentEditable.js
+++ packages/components/Common/ContentEditable.js
@@ -18,7 +18,7 @@ class ReadOnlyContentEditable extends React.Component {
         style={{ outline: 'none' }}
         onCut={() => false}
         onPaste={() => false}
-        onKeyDown={() => {
+        onKeyDown={event => {
           return !event.metaKey ? event.preventDefault() : true;
         }}
         innerRef={this.contentEditable}

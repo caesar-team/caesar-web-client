47baf3ca5506715a541c355974bad4b4ec9109e8
Sergey Alekseyev <sa@4xxi.com>
2020-07-09 13:59:14 +0600

    [MasterPass] CAES-964: Updated layout
    Signed-off-by: Sergey Alekseyev <sa@4xxi.com>

--- packages/components/Layout/Footer.js
+++ packages/components/Layout/Footer.js
@@ -1,28 +1,3 @@
-import React, { Fragment } from 'react';
-import styled from 'styled-components';
-import { Icon } from '../Icon';
-
-const FourXXIText = styled.div`
-  font-size: 12px;
-  font-weight: 600;
-  color: ${({ theme }) => theme.color.black};
-  margin-left: 10px;
-`;
-
-const FourXXILink = styled.a`
-  text-decoration: underline;
-  font-weight: 600;
-  color: ${({ theme }) => theme.color.black};
-`;
-
-const Footer = (
-  <Fragment>
-    <Icon name="logo-4xxi" width={20} height={20} />
-    <FourXXIText>
-      Created and supported by{' '}
-      <FourXXILink href="https://4xxi.com/en">4xxi team</FourXXILink>
-    </FourXXIText>
-  </Fragment>
-);
+const Footer = null;
 
 export default Footer;

fd2d0004e62a710f162d1c24e36dadc9673adc49
cu <cu@caesar.team>
2020-07-15 17:16:19 +0300

    [Item] Delete redundant Item component

--- packages/components/Item/Types/Document/schema.js
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-import { SCHEMA, ERROR } from '@caesar/common/validation';
-
-export const schema = yup.object({
-  name: yup.string().required(ERROR.REQUIRED_TEXT),
-  note: yup.string(),
-  attachments: SCHEMA.ATTACHMENTS,
-});

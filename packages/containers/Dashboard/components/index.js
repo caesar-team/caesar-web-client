e6b7385f5ef89d8842fe131d0876c25fa9b942b2
Sergey Alekseyev <sa@4xxi.com>
2020-07-15 16:13:04 +0600

    [Items] CAES-997: Fixed linter
    Signed-off-by: Sergey Alekseyev <sa@4xxi.com>

--- packages/containers/Dashboard/components/index.js
+++ packages/containers/Dashboard/components/index.js
@@ -1,5 +1,4 @@
 export { ConfirmMoveToTrashModal } from './ConfirmMoveToTrashModal';
 export { ConfirmRemoveItemModal } from './ConfirmRemoveItemModal';
 export { MiddleColumn } from './MiddleColumn';
-export { MoveModal } from './MoveModal';
 export { ShareModal } from './ShareModal';

2202ca1551a1e09288263c0d439b46e226867135
Sergey Alekseyev <sa@4xxi.com>
2020-07-15 19:13:20 +0600

    [Items] CAES-997: Fixed build
    Signed-off-by: Sergey Alekseyev <sa@4xxi.com>

--- packages/components/ItemNew/components/index.js
+++ packages/components/ItemNew/components/index.js
@@ -1,5 +1,4 @@
 export { ItemHeader } from './ItemHeader';
-export { MoveModal } from './MoveModal';
 export { OwnerAndInvitation } from './OwnerAndInvitation';
 export { RemoveButton } from './RemoveButton';
 export { Wrapper } from './Wrapper';
--- packages/containers/Dashboard/components/index.js
+++ packages/containers/Dashboard/components/index.js
@@ -1,4 +1,5 @@
 export { ConfirmMoveToTrashModal } from './ConfirmMoveToTrashModal';
 export { ConfirmRemoveItemModal } from './ConfirmRemoveItemModal';
+export { MoveModal } from './MoveModal';
 export { MiddleColumn } from './MiddleColumn';
 export { ShareModal } from './ShareModal';

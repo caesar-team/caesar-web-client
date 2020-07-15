201073966d6004e800a5b84ef594b29bc807c38d
Sergey Alekseyev <sa@4xxi.com>
2020-07-15 16:04:50 +0600

    [Items] CAES-997: Fixed linter
    Signed-off-by: Sergey Alekseyev <sa@4xxi.com>

--- packages/containers/Dashboard/components/MoveModal.js
+++ packages/containers/Dashboard/components/MoveModal.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import {
   workInProgressItemIdsSelector,
   workInProgressItemsSelector,
   workInProgressListIdSelector,
 } from '@caesar/common/selectors/workflow';
 import { currentTeamSelector } from '@caesar/common/selectors/user';
-import { resetWorkInProgressItemIds } from '@caesar/common/actions/workflow';
 import { MoveModal as MoveModalComponent } from '@caesar/components';
 
 export const MoveModal = ({
@@ -14,7 +13,6 @@ export const MoveModal = ({
   handleCloseModal,
   handleCtrlSelectionItemBehaviour,
 }) => {
-  const dispatch = useDispatch();
   const workInProgressItems = useSelector(workInProgressItemsSelector);
   const workInProgressItemIds = useSelector(workInProgressItemIdsSelector);
   const workInProgressListId = useSelector(workInProgressListIdSelector);

8413e13c09bed8db160a3f75a5ff7d7266f38ac9
Sergey Alekseyev <sa@4xxi.com>
2020-07-28 12:56:19 +0600

    [Lists] CAES-1085: Fix favorite items
    Signed-off-by: Sergey Alekseyev <sa@4xxi.com>

--- packages/common/selectors/entities/list.js
+++ packages/common/selectors/entities/list.js
@@ -132,8 +132,7 @@ export const favoriteListSelector = createSelector(
   (lists, trash, items) => {
     const favoriteList =
       lists.find(({ id, type }) => type === LIST_TYPE.FAVORITES) || {};
-//console.log(favoriteList);
-//console.log(items);
+
     return {
       ...favoriteList,
       children: favoriteList.children?.filter(

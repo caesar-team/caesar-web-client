5ec652678b77ed3dd592b3111eb5866383b7864a
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2020-08-20 12:45:38 +0300

    remove the commented lines
    Signed-off-by: Aleksandr Beshkenadze <beshkenadze@gmail.com>

--- packages/common/utils/base64.js
+++ packages/common/utils/base64.js
@@ -1,14 +1,8 @@
-// import btoa from 'btoa';
-// import atob from 'atob';
-
 export const objectToBase64 = object => {
   try {
-    const encodedString = unescape(encodeURIComponent(JSON.stringify(object)));
-
-    return Buffer.from(encodedString).toString('base64');
-    // return typeof window === 'undefined'
-    //   ? btoa(encodedString)
-    //   : window.btoa(encodedString);
+    return Buffer.from(
+      unescape(encodeURIComponent(JSON.stringify(object))),
+    ).toString('base64');
   } catch (ex) {
     // eslint-disable-next-line no-console
     console.error(ex);
@@ -22,9 +16,6 @@ export const base64ToObject = b64 => {
     return JSON.parse(
       decodeURIComponent(escape(Buffer.from(b64, 'base64').toString())),
     );
-    // return typeof window === 'undefined';
-    // ? JSON.parse(decodeURIComponent(escape(atob(b64))))
-    // : JSON.parse(decodeURIComponent(escape(window.atob(b64))));
   } catch (ex) {
     // eslint-disable-next-line no-console
     console.error(ex);

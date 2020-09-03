e0fad75e39a369983145174ae74e37c69b49d2f4
cu <cu@caesar.team>
2020-09-03 14:21:54 +0300

    [Refactoring] CAES-1183: Add AutoSizer and Virtual Scrolling List

--- packages/components/List/FixedSizeItem.js
+++ packages/components/List/FixedSizeItem.js
@@ -2,8 +2,11 @@ import React, { memo } from 'react';
 import { areEqual } from 'react-window';
 import { Item } from './Item';
 
-const FixedSizeItemComponent = ({ style, ...item }) => (
-  <Item style={style} key={item.id} {...item} />
-);
+const FixedSizeItemComponent = ({ data, index, style }) => {
+  const { items, ...itemProps } = data;
+  const item = items[index];
+
+  return <Item style={style} key={item.id} {...item} {...itemProps} />;
+};
 
 export const FixedSizeItem = memo(FixedSizeItemComponent, areEqual);

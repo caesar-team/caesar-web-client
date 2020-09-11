ecb4bfcf54e35fef9c77aad158b659d282f688eb
cu <cu@caesar.team>
2020-09-11 16:59:52 +0300

    [PassGenerator] CAES-1030: Make PasswordGeneratorComponent; implement generator into create item

--- packages/components/ItemFields/create/Password.js
+++ packages/components/ItemFields/create/Password.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import zxcvbn from 'zxcvbn';
 import styled from 'styled-components';
+import { TooltipPasswordGenerator } from '../../TooltipPasswordGenerator';
 import { PasswordInput } from '../../Input';
 import { PasswordIndicator } from '../../PasswordIndicator';
+
+const StyledTooltipPasswordGenerator = styled(TooltipPasswordGenerator)`
+  margin-right: 8px;
+`;
 
 const StyledPasswordInput = styled(PasswordInput)`
   ${PasswordInput.InputField} {
@@ -11,17 +16,31 @@ const StyledPasswordInput = styled(PasswordInput)`
 `;
 
 const StyledPasswordIndicator = styled(PasswordIndicator)`
-  margin-right: 16px;
+  margin-right: 8px;
 `;
 
-const Password = ({ value, ...props }) => (
-  <StyledPasswordInput
-    value={value}
-    addonPostfix={
-      value && <StyledPasswordIndicator score={zxcvbn(value).score} />
-    }
-    {...props}
-  />
-);
+const Password = ({ name, value, setFieldValue, ...props }) => {
+  const handleGeneratePassword = setValue => password =>
+    setValue(name, password);
+
+  return (
+    <>
+      <StyledPasswordInput
+        name={name}
+        value={value}
+        addonPostfix={
+          <>
+            {value && <StyledPasswordIndicator score={zxcvbn(value).score} />}
+            <StyledTooltipPasswordGenerator
+              onGeneratePassword={handleGeneratePassword(setFieldValue)}
+              tooltipProps={{ position: 'left center' }}
+            />
+          </>
+        }
+        {...props}
+      />
+    </>
+  );
+};
 
 export { Password };

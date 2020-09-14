df5347a63b635af6a385076f599e61f3a3b7d1ed
cu <cu@caesar.team>
2020-09-14 19:47:30 +0300

    [Attach] CAES-679: Add popup when remove file

---
+++ packages/components/ItemFields/view/ConfirmDeleteAttachmentModal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ConfirmModal } from '../../ConfirmModal';
+
+export const ConfirmDeleteAttachmentModal = ({
+  isOpened,
+  fileName,
+  onDeleteFile,
+  handleCloseModal,
+}) => (
+  <ConfirmModal
+    isOpened={isOpened}
+    title="You are going to delete the attachment"
+    description={`Are sure sure you want to delete "${fileName}"?`}
+    icon="trash"
+    confirmBtnText="Delete"
+    onClickConfirm={onDeleteFile}
+    onClickCancel={handleCloseModal}
+  />
+);

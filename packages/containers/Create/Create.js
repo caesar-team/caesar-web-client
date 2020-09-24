ee5f7f683bf37d64e17153317672784802bb48ed
cu <cu@caesar.team>
2020-09-24 13:20:27 +0300

    CAES-1304: Fix dropdown options when choose item path

--- packages/containers/Create/Create.js
+++ packages/containers/Create/Create.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useEffectOnce } from 'react-use';
+import { useDispatch } from 'react-redux';
+import { initWorkflow } from '@caesar/common/actions/workflow';
 import { CreateLayout, CreateForm } from '@caesar/components';
 
-export const Create = () => (
-  <CreateLayout>
-    <CreateForm />
-  </CreateLayout>
-);
+export const Create = () => {
+  const dispatch = useDispatch();
+
+  useEffectOnce(() => {
+    dispatch(initWorkflow());
+  });
+
+  return (
+    <CreateLayout>
+      <CreateForm />
+    </CreateLayout>
+  );
+};

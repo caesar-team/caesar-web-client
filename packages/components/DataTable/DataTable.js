5bfdc81661cadcb55f7f169db29bf3260302c3d4
cu <cu@caesar.team>
2020-09-25 15:19:34 +0300

    Delete react-table v6

--- packages/components/DataTable/DataTable.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-// @Deprecated. Use 'react-table' instead (v7)
-import ReactDataTable from 'react-table-6';
-import 'react-table-6/react-table.css';
-
-export const DataTable = props => <ReactDataTable {...props} />;

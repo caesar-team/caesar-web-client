44358c86670a010838bdfde00bb6594f98e37426
cu <cu@caesar.team>
2020-10-16 14:17:31 +0300

    [Logout] CAES-1356: Fix logout runtime errors

--- packages/common/utils/team.js
+++ packages/common/utils/team.js
@@ -1,7 +1,7 @@
 import { TEAM_TYPE } from '@caesar/common/constants';
 import { getDomainName } from './getDomainName';
 
-export const getTeamTitle = ({ type, title }) =>
+export const getTeamTitle = ({ type, title } = {}) =>
   type === TEAM_TYPE.DEFAULT || title?.toLowerCase() === TEAM_TYPE.DEFAULT
     ? getDomainName(window.location.hostname)
     : title;

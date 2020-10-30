da515212c23ccc06db58cb6b7dd8ec8864cd245e
Aleksandr Beshkenadze <ab@caesar.team>
2020-10-30 17:05:52 +0300

    Free threads after completed all jobs (#577)
    * WIP
    
    * Free threads after complete all jobs

--- packages/common/utils/worker.js
+++ packages/common/utils/worker.js
@@ -1,11 +1,13 @@
 import { isClient } from '@caesar/common/utils/isEnvironment';
 
-const DEFAULT_LOGICAL_CORES_COUNT = 4;
+const DEFAULT_LOGICAL_CORES_COUNT = 1;
 
 // 2 * n + 1 is used here(where n is a count of physical cores,
 // 2 * n gives us a count of logical cores),
 // 1 web worker is being used by openpgp package
 // hardwareConcurrency returns the number of logical cores
 export function getWorkersCount() {
-  return isClient ? navigator.hardwareConcurrency : DEFAULT_LOGICAL_CORES_COUNT;
+  return (
+    (isClient ? navigator.hardwareConcurrency : DEFAULT_LOGICAL_CORES_COUNT) - 1
+  );
 }

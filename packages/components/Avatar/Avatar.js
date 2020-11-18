c3d589b37d8563cd5ec80fff230826da3d8eb820
Aleksandr Beshkenadze <ab@caesar.team>
2020-11-18 13:43:30 +0300

    Fix avatars and showing menu, confirm to remove (#630)
    * Fix avatars and showing menu, confirm to remove
    
    Signed-off-by: Aleksandr Beshkenadze <ab@caesar.team>
    
    * ¯\_(ツ)_/¯
    
    Signed-off-by: Aleksandr Beshkenadze <ab@caesar.team>

--- packages/components/Avatar/Avatar.js
+++ packages/components/Avatar/Avatar.js
@@ -1,40 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
 import { API_URI } from '@caesar/common/constants';
 import { Hint } from '../Hint';
-import { Icon } from '../Icon';
-
-const Wrapper = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: ${({ size }) => size}px;
-  min-width: ${({ size }) => size}px;
-  flex: 0 0 ${({ size }) => size}px;
-  height: ${({ size }) => size}px;
-  padding: 0;
-  margin: 0;
-  overflow: hidden;
-  font-size: ${({ fontSize, theme }) => theme.font.size[fontSize]};
-  color: ${({ theme }) => theme.color.white};
-  white-space: nowrap;
-  text-align: center;
-  background-color: ${({ theme }) => theme.color.gray};
-  border-radius: 50%;
-`;
-
-const Image = styled.img`
-  width: 100%;
-  height: 100%;
-  border-style: none;
-`;
+import { Wrapper, Image } from './styles';
 
 export const Avatar = ({
   name,
   email,
   avatar,
-  accessGranted,
   children,
   size = 40,
   fontSize = 'main',
@@ -46,10 +18,6 @@ export const Avatar = ({
     switch (true) {
       case !!children:
         return children;
-
-      case !accessGranted: {
-        return <Icon name="warning" width={40} height={40} color="black" />;
-      }
 
       case !!avatar: {
         const avatarIcon = avatar.startsWith('data:')
@@ -68,7 +36,6 @@ export const Avatar = ({
       }
     }
   };
-
   return (
     <Hint text={hint} position={hintPosition}>
       <Wrapper size={size} fontSize={fontSize} {...props}>

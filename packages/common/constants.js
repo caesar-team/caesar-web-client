3ffa8a68e6bfe4e7cf066784964f0959a8d46d80
Aleksandr Beshkenadze <ab@caesar.team>
2020-11-30 18:04:03 +0300

    The secure link now has less length (#657)
    * WIP
    
    * Make the secure link length less

--- packages/common/constants.js
+++ packages/common/constants.js
@@ -124,7 +124,7 @@ export const ROUTES = {
   LOGOUT: '/logout',
   RESETTING: '/resetting',
   SECURE: '/secure',
-  SECURE_MESSAGE: '/secure/message/[id]',
+  SECURE_MESSAGE: '/s/m/[id]',
   SHARE: '/share',
   INVITE: '/invite',
   DASHBOARD: '/',
@@ -199,7 +199,7 @@ export const SAVE_NOTIFICATION = 'Saving...';
 export const NOOP_NOTIFICATION = '';
 
 export const UUID_REGEXP = /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/;
-
+export const SECURE_MESSAGE_REGEXP = /(\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b)+,(.*)/;
 export const TEAM_AVATAR_MAX_SIZE = 8 * 1024 * 1024;
 
 export const GOOD_PASSWORD_SCORE = 3;
@@ -265,7 +265,7 @@ export const TEAM_ROLES_OPTIONS =
   Object.values(TEAM_ROLES).map(role => ({
     value: role,
     label: TEAM_ROLES_LABELS[role],
-  }));
+}));
 
 export const PERMISSION_MESSAGES = {
   FORBIDDEN_SELECT: "You don't have permissions to select the item",

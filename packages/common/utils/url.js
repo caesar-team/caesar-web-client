fe375f0c44171c50e9644d73aa8ff4ce28097b43
Sergey Alekseyev <sa@4xxi.com>
2020-12-01 13:27:18 +0600

    [Items] CAES-1538: Fix domains for favicons
    Signed-off-by: Sergey Alekseyev <sa@4xxi.com>

---
+++ packages/common/utils/url.js
@@ -0,0 +1,22 @@
+export const parseUrl = url => {
+  let prefix = '';
+
+  if (!/^https:\/\//.test(url)) {
+    prefix = 'https://';
+  } 
+    
+  try {
+    const urlObject = new URL(prefix + url);
+    
+    return urlObject;
+  } catch(error) {
+    console.warn(`URL ${url} is invalid`);
+    return {};
+  }
+}
+
+export const getOriginDomain = url => {
+  const parsedUrl = parseUrl(url);
+  
+  return parsedUrl.hostname;
+}; 

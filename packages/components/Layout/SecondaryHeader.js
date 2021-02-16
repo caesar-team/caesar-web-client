23e4b3e59452371cfc811967844c06101beed1eb
Sergey Alekseyev <sa@4xxi.com>
2021-02-16 18:59:26 +0600

    [Layout] CAES-1668: Fix linter error
    Signed-off-by: Sergey Alekseyev <sa@4xxi.com>

--- packages/components/Layout/SecondaryHeader.js
+++ packages/components/Layout/SecondaryHeader.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { withRouter } from 'next/router';
 import { useMedia } from '@caesar/common/hooks';

350015ea64bc93232d404ddb442b7b2bc0174048
Mikhail <mbaev@4xxi.com>
2022-01-10 18:10:43 +0700

    feature: added button import

--- packages/web-app-new/pages/index.tsx
+++ packages/web-app-new/pages/index.tsx
@@ -1,4 +1,5 @@
 import { styled } from 'linaria/react';
+import { Button } from '@caesar/components';
 
 const Wrapper = styled.div`
   color: white;
@@ -8,6 +9,7 @@ const Wrapper = styled.div`
 const Home = () => {
   return (
     <Wrapper>
+      <Button>wfwe</Button>
       <span> web-app-new</span>
     </Wrapper>
   );

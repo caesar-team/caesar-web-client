9ffa3316d972ad2902a795fe0a30a5a938e92331
Mikhail <mbaev@4xxi.com>
2022-01-24 17:50:35 +0700

    feature: added global styles and fonts for secure-app-new

--- packages/assets/styles/globalStyles.js
+++ packages/assets/styles/globalStyles.js
@@ -1,52 +1,60 @@
 import { normalize } from 'polished';
-import theme from '@caesar/common/theme';
-import fonts from './fonts';
+import { css } from 'linaria';
 
-export default `
-  ${normalize()};
-  ${fonts};
+export default css`
+  :global() {
+    @font-face {
+      font-family: 'AvenirNext';
+      font-weight: normal;
+      font-style: normal;
+      src: url(/fonts/AvenirNext-Regular.woff) format('woff'),
+        url(/fonts/AvenirNext-Regular.woff2) format('woff2'),
+        url(/fonts/AvenirNext-Regular.ttf) format('ttf'),
+        url(/fonts/AvenirNext-Regular.eot) format('eot');
+    }
 
-  html {
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-font-smoothing: antialiased;
-    background-color: ${theme.color.white};
-    min-width: 20rem;
-    font-family: 'AvenirNext';
-    font-weight: normal;
-    font-style: normal;
-    font-size: ${theme.font.size.main};
-    line-height: ${theme.font.lineHeight.main};
-    letter-spacing: 0.4px;
-  }
+    html {
+      -moz-osx-font-smoothing: grayscale;
+      -webkit-font-smoothing: antialiased;
+      background-color: #fff;
+      min-width: 20rem;
+      font-family: 'AvenirNext';
+      font-weight: normal;
+      font-style: normal;
+      font-size: 16px;
+      line-height: 1.5;
+      letter-spacing: 0.4px;
+    }
 
-  button,
-  input,
-  optgroup,
-  select,
-  textarea {
-    font-family: 'AvenirNext';
-    font-weight: normal;
-    font-style: normal;
-  }
+    button,
+    input,
+    optgroup,
+    select,
+    textarea {
+      font-family: 'AvenirNext';
+      font-weight: normal;
+      font-style: normal;
+    }
 
-  html,
-  body,
-  #root {
-    height: 100%;
-    margin: 0;
-  }
+    html,
+    body,
+    #root {
+      height: 100%;
+      margin: 0;
+    }
 
-  *,
-  *::before,
-  *::after {
-    box-sizing: border-box;
-  }
+    *,
+    *::before,
+    *::after {
+      box-sizing: border-box;
+    }
 
-  div {
-    user-select: none;
-  }
+    div {
+      user-select: none;
+    }
 
-  .modal-is-opened {
-    overflow: hidden;
+    .modal-is-opened {
+      overflow: hidden;
+    }
   }
 `;
